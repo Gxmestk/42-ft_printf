e2f15bca732133b1765a5c2fcb3bf75a4887cc78
Gxmestk <gt.khemniwat@gmail.com>
2022-09-10 11:42:47 +0700

    fix printf_wdt when arr[0] - arr[1]  == 1 this ok for other cases

--- srcs/main.c
+++ srcs/main.c
@@ -5,7 +5,8 @@ int main()
 	printf("\n\n");
 	//int a = ft_printf("%.10d",LONG_MAX);
 	//int a = ft_printf("x%-.dx", 0);
-	int a =   ft_printf("%8p is the address", (void *)0);
+	int a =   ft_printf("the address is %13p", (void *)0x7ffe6b8e60c8);
+	//int a =   ft_printf("%#3x", 255);
 	//int a =ft_printf("game\n");
 	printf("\n\nlen  = %d\n",a);
 }
