829adb5329575074b9140a587be9c855d4e3b111
Gxmestk <gt.khemniwat@gmail.com>
2022-09-11 02:57:31 +0700

    ft_printf

--- srcs/print_by_format.c
+++ srcs/print_by_format.c
@@ -6,7 +6,7 @@
 /*   By: tkhemniw <gt.khemniwat@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 03:01:00 by tkhemniw          #+#    #+#             */
-/*   Updated: 2022/09/11 02:13:15 by tkhemniw         ###   ########.fr       */
+/*   Updated: 2022/09/11 02:57:25 by tkhemniw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ int	print_by_format(char c, va_list args, t_format *f)
 {
 	char	*str;
 
-	if(f->wdt > 2147483640 || f->pcs > 2147483640)
-		return (0);
 	str = NULL;
 	if (c == 'c')
 		str = print_c(va_arg(args, int), f);
@@ -35,6 +33,11 @@ int	print_by_format(char c, va_list args, t_format *f)
 		str = print_x(va_arg(args, unsigned int), f, 1);
 	else if (c == '%')
 		str = print_c('%', f);
+	if (f->len_f > 2147480000)
+	{
+		free(str);
+		return (-1);
+	}
 	write(1, str, f->len_f);
 	free(str);
 	return (f->len_f);
