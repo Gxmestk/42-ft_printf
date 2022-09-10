e67e19bfa8db664d0c4175ae14bb6a689ea3bd31
Gxmestk <gt.khemniwat@gmail.com>
2022-09-11 00:05:14 +0700

    ft_strlen

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: tkhemniw <gt.khemniwat@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 12:56:28 by tkhemniw          #+#    #+#             */
-/*   Updated: 2022/09/10 12:58:21 by tkhemniw         ###   ########.fr       */
+/*   Updated: 2022/09/11 00:04:15 by tkhemniw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_strlen(const char *str)
 	int	i;
 
 	i = 0;
+	if (!str)
+		return (NULL);
 	while (str[i])
 		i++;
 	return (i);
