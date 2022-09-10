e0fbaaf6c1ea8380a0e66695349b9fb8beae4330
Gxmestk <gt.khemniwat@gmail.com>
2022-09-10 14:35:55 +0700

    S and C

--- srcs/add_char_content.c
+++ srcs/add_char_content.c
@@ -6,7 +6,7 @@
 /*   By: tkhemniw <gt.khemniwat@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 13:33:33 by tkhemniw          #+#    #+#             */
-/*   Updated: 2022/09/10 14:33:35 by tkhemniw         ###   ########.fr       */
+/*   Updated: 2022/09/10 14:35:46 by tkhemniw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,14 @@ void	add_char_content(char **str, char c, t_format *f)
 	if (f->minus)
 	{
 		**str = c;
-		*str++;
-		*str = (char *)ft_memset_last(*str, ' ', f->wdt
-				- lenw);
+		(*str)++;
+		*str = (char *)ft_memset_last(*str, ' ', f->wdt - 1);
 	}
 	else
 	{
-		*str = (char *)ft_memset_last(*str, ' ', f->wdt
-				- lenw);
+		*str = (char *)ft_memset_last(*str, ' ', f->wdt - 1);
 		**str = c;
-		*str++;
+		(*str)++;
 	}
 	**str = '\0';
 }
