fe8542c4018e4098b4fda6956219cf825328d8e4
Gxmestk <gt.khemniwat@gmail.com>
2022-09-10 11:57:26 +0700

    ft_printf

--- srcs/print_wdt.c
+++ srcs/print_wdt.c
@@ -6,7 +6,7 @@
 /*   By: tkhemniw <gt.khemniwat@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 21:10:22 by tkhemniw          #+#    #+#             */
-/*   Updated: 2022/09/10 11:50:50 by tkhemniw         ###   ########.fr       */
+/*   Updated: 2022/09/10 11:57:07 by tkhemniw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ void	print_wdt(char **str, t_format *f)
 		w = '0';
 	*str = (char *)ft_memset_last(*str, w, f->wdt
 			- f->arr[1] - f->prefix_len);
-	if (f->wdt - f->arr[1] > 0)
+	if (f->wdt - f->arr[1] - f->prefix_len < 0)
+		f->len_f = f->arr[1] + f->prefix_len;
+	else if (f->wdt - f->arr[1] > 0)
 		f->len_f -= f->prefix_len;
 }
