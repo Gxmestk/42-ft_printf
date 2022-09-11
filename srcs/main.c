29fb5959d460502d5291b08771fc0801af980762
Gxmestk <gt.khemniwat@gmail.com>
2022-09-11 18:52:01 +0700

    ft_printf

--- srcs/main.c
+++ srcs/main.c
@@ -2,15 +2,38 @@
 
 int main()
 {
+	int		a = -4;
+	//int		b = 0;
+	char	c = 'a';
+	int		d = 2147483647;
+	int		e = -2147483648;
+	//int		f = 42;
+	//int		g = 25;
+	//int		h = 4200;
+	int		i = 8;
+	int		j = -12;
+	int		k = 123456789;
+	int		l = 0;
+	int		m = -12345678;
+	//char	*n = "abcdefghijklmnop";
+	//char	*o = "-a";
+	//char	*p = "-12";
+	//char	*q = "0";
+	//char	*r = "%%";
+	//char	*s = "-2147483648";
+	//char	*t = "0x12345678";
+	//char	*u = "-0";
+
 	printf("\n\n");
 	//int a = ft_printf("%.10d",LONG_MAX);
 	//int a = ft_printf("x%-.dx", 0);
 	//int a =   ft_printf("the address is %13p", (void *)0x7ffe6b8e60c8);
 	//int a =   ft_printf("the address is %20p", (void *)0xffffffffffffffff);
 	//int a = ft_printf("%1s", NULL);
-	int a = ft_printf("xxx%+.2147483600dxxx",5);
+	//int a = ft_printf("xxx%+.2147483600dxxx",5);
+	int XXX = ft_printf("%*i, %*d, %*d, %*d, %*d, %*d, %*d, %*d",-4, 0, a, i, a, j, a, k, a, l, a, m, a, c, a, e, a, d);
 	//int a = ft_printf("xxx%2147483646dxxx",5);
 	//int a =ft_printf("game\n");
-	printf("\n\nlen  = %d\n",a);
+	printf("\n\nlen  = %d\n",XXX);
 }
 /*   (void *)0xffffffffffffffff NULL (void *)0x8000000000000000   */
--- srcs/print_s.c
+++ srcs/print_s.c
@@ -6,12 +6,12 @@
 /*   By: tkhemniw <gt.khemniwat@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 18:49:04 by tkhemniw          #+#    #+#             */
-/*   Updated: 2022/09/11 03:37:58 by tkhemniw         ###   ########.fr       */
+/*   Updated: 2022/09/11 18:51:55 by tkhemniw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
-#include <string.h>
+#include <string.h>//
 
 int	lenw(char *s, t_format *f)
 {	
@@ -19,7 +19,7 @@ int	lenw(char *s, t_format *f)
 
 	if (!s)
 		return (0);
-	s_len = strlen(s);
+	s_len = 5;//strlen(s);
 	if (f->p && f->pcs < s_len)
 		return (f->pcs);
 	return (s_len);
