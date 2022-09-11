c7cff00ca52754479d502ad5be5eb630a92e4823
Gxmestk <gt.khemniwat@gmail.com>
2022-09-11 22:09:12 +0700

    ft_printf

--- srcs/main.c
+++ srcs/main.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-	int		a = -4;
+	/*int		a = -4;
 	int		b = 0;
 	char	c = 'a';
 	int		d = 2147483647;
@@ -25,37 +25,11 @@ int main()
 	//char	*u = "-0";
 	int		l = 0;
 	//int		m = -12345678;
-	//char	*n = "abcdefghijklmnop";
+	//char	*n = "abcdefghijklmnop";*/
 	
-	printf("\n\n");
-	//int a = ft_printf("%.10d",LONG_MAX);
-	//int a = ft_printf("x%-.dx", 0);
-	//int a =   ft_printf("the address is %13p", (void *)0x7ffe6b8e60c8);
-	//int a =   ft_printf("the address is %20p", (void *)0xffffffffffffffff);
-	//int a = ft_printf("%1s", NULL);
-	//int a = ft_printf("xxx%+.2147483600dxxx",5);
-	/*a = -2;
-	while(a < 5) //T70-177
-	{
-		b = -2;
-		while (b < 5)
-		{
-			ft_printf("%*.*s, %*.*s, %*.*s, %*.*s, %*.*s, %*.*s, %*.*s, %*.*s", a, b, n, a, b, o, a, b, p, a, b, q, a, b, r, a, b, s, a, b, t, a, b, u);
-			ft_printf("%-*.*s, %-*.*s, %-*.*s, %-*.*s, %-*.*s, %-*.*s, %-*.*s, %-*.*s", a, b, n, a, b, o, a, b, p, a, b, q, a, b, r, a, b, s, a, b, t, a, b, u);
-			b++;
-		}
-		a++;
-	}*/ //n  o  p  q  r  s  t  u
-	a = 2;
-	b = 2;
-	//int a =  1234;
-	//ft_printf("x%*dx\n",2,a);
-	/*int XXX = printf("%*.*s, %*.*s, %*.*s, %*.*s, %*.*s, %*.*s, %*.*s, %*.*s\n", a, b, n, a, b, o, a, b, p, a, b, q, a, b, r, a, b, s, a, b, t, a, b, u);
-	//int a = ft_printf("xxx%2147483646dxxx",5);
-	//int a =ft_printf("game\n");
-	printf("\n\nlen  = %d\n",XXX);*/
+	
 
-	a = -2;
+	/*a = -2;
 	while(a < 5) //T70-177
 	{
 		b = -2;
@@ -67,6 +41,8 @@ int main()
 			b++;
 		}
 		a++;
-	}
+	}*/
+	printf("\n");
+	ft_printf("x%*.*ix",2,-1,8);
 }
 /*   (void *)0xffffffffffffffff NULL (void *)0x8000000000000000   */
